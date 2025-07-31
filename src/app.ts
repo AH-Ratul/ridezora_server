14343f87d43259953cda92b31348b5c6108bb311
AH-Ratul <ahratul21@gmail.com>
2025-07-31 20:23:04 +0600

    feat: add app route with versioning

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
+import { appRoute } from "./app/route";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1", appRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("RideZora app running...");
