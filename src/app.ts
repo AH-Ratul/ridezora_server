b4a65ad8984d20efeb40b95b102d0a0496083104
AH-Ratul <ahratul21@gmail.com>
2025-07-31 21:35:49 +0600

    feat: add globalErrorHandler & notFound to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { appRoute } from "./app/route";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app = express();
 
@@ -10,5 +12,9 @@ app.use("/api/v1", appRoute);
 app.get("/", (req: Request, res: Response) => {
   res.send("RideZora app running...");
 });
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
