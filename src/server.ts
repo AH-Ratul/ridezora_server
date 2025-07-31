e322c80ac79d8576a246ca7b76c365813c91f935
AH-Ratul <ahratul21@gmail.com>
2025-07-31 18:43:25 +0600

    setup complete

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import { env } from "./app/config/env";
+import app from "./app";
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(env.DB_URL as string);
+    console.log("Database Connected");
+
+    server = app.listen(env.PORT, () => {
+      console.log(`Server is listening on Port ${env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Server Error -> ", error);
+  }
+}
+
+main();
+
+// handling unhandled rejection
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandeld Rejection detected.. shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log("Uncought Exception detected.. Shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
