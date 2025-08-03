8d18791f58d156d2d9e46463d7e5fede8f08837f
AH-Ratul <ahratul21@gmail.com>
2025-08-03 20:43:50 +0600

    update: salt type

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,10 +1,36 @@
-import { IUser } from "./user.interface";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+import bcrypt from "bcryptjs";
+import { env } from "../../config/env";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email, phone, role } = payload;
+  const { email, password, ...rest } = payload;
 
-  const user = await User.create({ name, email, phone, role });
+  const isUserExist = await User.findOne({ email });
+
+  if (isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Email Already Exists");
+  }
+
+  // hash the password
+  const hashedPassword = await bcrypt.hash(
+    password as string,
+    Number(env.SALT)
+  );
+
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
+
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+    auths: authProvider,
+    ...rest,
+  });
 
   return user;
 };
