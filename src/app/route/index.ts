64975e031f6c69134066e77f07d15a3b60d72693
AH-Ratul <ahratul21@gmail.com>
2025-08-03 19:23:24 +0600

    add new route

--- src/app/route/index.ts
+++ src/app/route/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { userRoute } from "../modules/user/user.route";
+import { authRoute } from "../modules/auth/auth.route";
 
 export const appRoute = Router();
 
@@ -8,6 +9,10 @@ const moduleRoutes = [
     path: "/user",
     route: userRoute,
   },
+  {
+    path: "/auth",
+    route: authRoute
+  }
 ];
 
 moduleRoutes.forEach((route) => {
