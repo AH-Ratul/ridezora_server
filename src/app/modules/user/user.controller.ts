7284161bb3dec2aa107d7a2e1bfd797804ff420c
AH-Ratul <ahratul21@gmail.com>
2025-08-05 14:00:05 +0600

    feat: manage get & update user controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { catchAsync } from "../../utils/catchAsync";
 import { UserService } from "./user.services";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.createUser(req.body);
@@ -15,6 +16,49 @@ const createUser = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.getAllUsers();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "All User Retrieved",
+    data: result,
+  });
+});
+
+const getSingleUser = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.getSingleUser(req.params.userId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "User Retrieved",
+    data: result,
+  });
+});
+
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const body = req.body;
+  const decodedToken = req.user;
+  const result = await UserService.updateUser(
+    userId,
+    body,
+    decodedToken as JwtPayload
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "User Updated",
+    data: result,
+  });
+});
+
 export const UserController = {
   createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
 };
