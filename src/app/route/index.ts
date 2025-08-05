fdd457a330234dc2990ee901f11aa606d1dfc154
AH-Ratul <ahratul21@gmail.com>
2025-08-05 21:25:13 +0600

    feat: new route added

--- src/app/route/index.ts
+++ src/app/route/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userRoute } from "../modules/user/user.route";
 import { authRoute } from "../modules/auth/auth.route";
+import { riderRoute } from "../modules/rider/rider.route";
 
 export const appRoute = Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: authRoute
+  },
+  {
+    path: "/rides",
+    route: riderRoute
   }
 ];
 
