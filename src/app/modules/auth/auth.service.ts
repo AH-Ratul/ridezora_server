52f663e59fe3ff5a8e3ed12481ba2ee64fa6ab64
AH-Ratul <ahratul21@gmail.com>
2025-08-05 13:02:20 +0600

    feat: handle reset password

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,1 +1,40 @@
+import { JwtPayload } from "jsonwebtoken";
+import { createAccessTokenWithRefreshToken } from "../../utils/userTokens";
+import { User } from "../user/user.model";
+import bcrypt from "bcryptjs";
+import AppError from "../../errorHelpers/AppError";
+import { env } from "../../config/env";
 
+const getNewAccessToken = async (refreshToken: string) => {
+  const newAccessToken = await createAccessTokenWithRefreshToken(refreshToken);
+
+  return {
+    accessToken: newAccessToken,
+  };
+};
+
+const resetPassword = async (
+  newPassword: string,
+  oldPassword: string,
+  decodedToken: JwtPayload
+) => {
+  const user = await User.findById(decodedToken.userId);
+
+  const isOldPasswordMatch = await bcrypt.compare(
+    oldPassword,
+    user!.password as string
+  );
+
+  if (!isOldPasswordMatch) {
+    throw new AppError(400, "Old Password does not match");
+  }
+
+  user!.password = await bcrypt.hash(newPassword as string, Number(env.SALT));
+
+  user!.save();
+};
+
+export const AuthService = {
+  getNewAccessToken,
+  resetPassword
+};
