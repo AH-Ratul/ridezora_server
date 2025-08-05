72a9fda8905e5da6283b03f168c5b85ba09409cf
AH-Ratul <ahratul21@gmail.com>
2025-08-05 13:59:31 +0600

    feat: handle get & update user

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,9 +1,10 @@
 import AppError from "../../errorHelpers/AppError";
-import { IAuthProvider, IUser } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
 import bcrypt from "bcryptjs";
 import { env } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = async (payload: Partial<IUser>) => {
   const { email, password, ...rest } = payload;
@@ -35,9 +36,78 @@ const createUser = async (payload: Partial<IUser>) => {
   return user;
 };
 
-const getAllUsers = async () => {};
+const getAllUsers = async () => {
+  const users = await User.find();
+
+  return users;
+};
+
+const getSingleUser = async (userId: string) => {
+  const user = await User.findById(userId);
+
+  return { data: user };
+};
+
+const updateUser = async (
+  userId: string,
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload
+) => {
+  const isUserExist = await User.findById(userId);
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "User Not Found");
+  }
+
+  if (payload.role) {
+    // block normer user from changing any roles
+    if (decodedToken.role === Role.RIDER || decodedToken.role === Role.DRIVER) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+
+    //prevent ADMIN from assigning the SUPER_ADMIN role
+    // only SUPER_ADMIN can assign the SUPER_ADMIN role
+    if (
+      decodedToken.role === Role.SUPER_ADMIN &&
+      decodedToken.role === Role.ADMIN
+    ) {
+      throw new AppError(
+        httpStatus.FORBIDDEN,
+        "Only SUPER_ADMIN can assign SUPER_ADMIN role"
+      );
+    }
+  }
+
+  if (
+    payload.isActive ||
+    payload.isDeleted ||
+    payload.isVerified ||
+    payload.approved
+  ) {
+    // block RIDER & DRIVER from change user status
+    if (
+      decodedToken.role === Role.DRIVER ||
+      decodedToken.role === Role.DRIVER
+    ) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.password) {
+    payload.password = await bcrypt.hash(payload.password, env.SALT);
+  }
+
+  const newUpdateUser = await User.findByIdAndUpdate(userId, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return newUpdateUser;
+};
 
 export const UserService = {
   createUser,
   getAllUsers,
+  getSingleUser,
+  updateUser,
 };
