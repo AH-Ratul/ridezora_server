4c7f8136ab9312216fb6f8f1fbebf2226fd76d52
AH-Ratul <ahratul21@gmail.com>
2025-08-08 16:11:17 +0600

    move driver routes to driver route file

--- src/app/modules/ride/ride.route.ts
+++ src/app/modules/ride/ride.route.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
-import { DriverController } from "../driver/driver.controller";
 import { RiderController } from "./ride.controller";
 
 export const rideRoute = Router();
 
-//-------- RIDER'S ROUTES -----------
 rideRoute.post("/request", checkAuth(Role.RIDER), RiderController.requestARide);
 rideRoute.patch(
   "/cancel/:id",
@@ -14,34 +12,3 @@ rideRoute.patch(
   RiderController.cancelARide
 );
 rideRoute.get("/me", checkAuth(Role.RIDER), RiderController.myRides);
-
-// ----- DRIVER'S ROUTES -----------
-rideRoute.get(
-  "/available",
-  checkAuth(Role.DRIVER),
-  DriverController.getAvailableRides
-);
-
-rideRoute.get(
-  "/driver-earnings",
-  checkAuth(Role.DRIVER),
-  DriverController.myRides
-);
-
-rideRoute.patch(
-  "/accept/:id",
-  checkAuth(Role.DRIVER),
-  DriverController.acceptRide
-);
-
-rideRoute.patch(
-  "/status/:id",
-  checkAuth(Role.DRIVER),
-  DriverController.updateRideStatus
-);
-
-rideRoute.patch(
-  "/cancel/:id",
-  checkAuth(Role.DRIVER),
-  DriverController.cancelARide
-);
